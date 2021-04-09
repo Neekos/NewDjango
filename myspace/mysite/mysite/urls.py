13916a5f6a4093b37d62c4e811e0ab10280674fa
Neek <Sp-Neek@yndex.ru>
2021-04-09 19:59:53 +0700

    прописываю первый маршрутизатор
    импортирую дополнительные библиотека для дальнейшей работы

--- myspace/mysite/mysite/urls.py
+++ myspace/mysite/mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import *
+from django.urls import path, include
+from mysite import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
