702ea3c620c80e02299fd6275b39b23fa1b2ad59
Neek <Sp-Neek@yndex.ru>
2021-04-09 20:07:00 +0700

    Создание путей и их отображение
    СОздаем пути прописываем библиотеки, и функции для их отображения

--- myspace/mysite/blog/views.py
+++ myspace/mysite/blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+form .models import *
 
 # Create your views here.
+
+def index():
+	return render(request, 'blog/index.html')
